8e8809856b46540522509c5c93c6247d55676ece
Marcian Diamond <marciandmnd@yahoo.com>
2013-09-30 17:15:21 -0700

    m

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'YAML'
+#require 'YAML'
 require 'rails/all'
 Bundler.require(:default, Rails.env)
 
 #open and load project.yml
 fileName = File.open('config/projects.yml', 'r')
 $projectData = YAML.load(fileName)
-puts $projectData.inspect
 
 module Proj1
   class Application < Rails::Application
