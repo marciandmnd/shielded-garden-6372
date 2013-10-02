a6ae9fb71e07f739b33036510a0077070f14b6a7
Marcian Diamond <marciandmnd@yahoo.com>
2013-10-02 12:06:44 -0700

    final proj1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
   	@current_time = Time.now
-  	@projectHash = $projectData.to_hash()
-  	@projectNames = @projectHash.keys
+  	#@projectHash = $projectData.to_hash()
+  	#@projectNames = @projectHash.keys
   end
 end
 #attr_reader :attr_names
